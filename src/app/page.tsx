ffd00cf80e51c46125f7f7e55e563795f0a70a6b
ShrimpDev <muhammadfaheemwahid@gmail.com>
2024-12-20 10:57:30 +0800

    update default home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-export default function Home({ children }: { children: React.ReactNode }) {
+export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[100vh] w-[100vw] ">
-      <h1 className="text-9xl font-extrabold text-lime-500">Welcome!</h1>
-      <p className="text-5xl text-lime-500 mt-10">
+    <div className="flex flex-col items-center justify-center min-h-[100vh] w-[100vw] text-center">
+      <h1 className="text-5xl md:text-9xl font-extrabold text-lime-500">
+        Welcome!
+      </h1>
+      <p className="text-xl md:text-5xl text-lime-500 mt-10">
         Please choose a task from the navbar!
       </p>
     </div>
